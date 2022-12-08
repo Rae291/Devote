75aeac825df2e7074c2ca072301e3b0a003b9e12
Rae291 <107467793+Rae291@users.noreply.github.com>
2022-12-08 11:04:19 +0530

    Add files via upload

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by APPLE on 08/12/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
